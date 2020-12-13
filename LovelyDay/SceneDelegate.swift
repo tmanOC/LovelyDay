e35a47b267e5ca796e2b216a2f91fdc1a6ac9ff7
Tielman Janse van Vuuren <vanvuuren.tielman@gmail.com>
2020-12-13 22:38:46 +0200

    Setup basic project, going with Scene Delegate and without main storyboard

--- LovelyDay/SceneDelegate.swift
+++ LovelyDay/SceneDelegate.swift
@@ -11,12 +11,25 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        if self.window == nil {
+            self.window = UIWindow.init(windowScene: windowScene);
+        } else {
+            self.window?.windowScene = windowScene
+        }
+        guard let appWindow = self.window else {
+            return
+        }
+        appWindow.backgroundColor = UIColor.white
+        appWindow.rootViewController = ViewController()
+        appWindow.makeKeyAndVisible()
+        // self.window?.tintColor = ColorTheme.bluePrimary
+        // self.window?.rootViewController = self.firstViewController()
+
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
